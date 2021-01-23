05efa606b7733c7890b15c6762485bfcb4d95b2a
evoskoboeva <GITHUB evoskoboeva@tango.com.ua>
2021-01-23 14:46:00 +0200

    Task1 My ASCII Cat

---
+++ src/com/companyCat/Main.java
@@ -0,0 +1,27 @@
+package com.companyCat;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Это моя кошка Катя в задании 1"+System.lineSeparator());
+        System.out.println("░░░░░█▄░░░░░░░░░░▄█░░░░░");
+        System.out.println("░░░░░████▄░░░░▄████░░░░░");
+        System.out.println("░░░░░██████████████░░░░░");
+        System.out.println("▄░░░░███▄▄████▄▄███░░░░▄");
+        System.out.println("░▀▀▀███████▄▄███████▀▀▀░");
+        System.out.println("▄▀▀▀▀▀██▄░░░░░░▄██▀▀▀▀▀▄");
+        System.out.println("░░░░░░▀███░░░░███▀░░░░░░");
+        System.out.println("░░░░░░░░▀██▄▄██▀░░░░░░░░");
+        System.out.println("░░░░░░▄█▄▄░░░░▄▄█▄░░░░░░");
+        System.out.println("░░░░▄██████████████▄░░░░");
+        System.out.println("░░░▄████████████████▄░░░");
+        System.out.println("░░████████████████████░░");
+        System.out.println("░██████▀▀██░░██▀▀██████░");
+        System.out.println("███████░░██░░██░░███████");
+        System.out.println("▀██████░░██░░██░░██████▀");
+        System.out.println("░████▀▀░▄██░░██▄░▀▀███▀░");
+        System.out.println("░░▀██░▄█▀██░░██▀█▄░██▀░░");
+        System.out.println("░░░░░░░▀░░█░░█░░▀░░░░░░░");
+           }
+}
